fc1352eccf39d1f6e19c90e08dd50102ac336de4
kgonzalez452 <kgonzalez@techlaunch.io>
2018-06-05 19:56:50 -0400

    completed challenge two, wasnt that bad to figure out, starting to learn how to use closure, somewhat. Explained challenge three in a way.

--- part2a/part2a.js
+++ part2a/part2a.js
@@ -4,29 +4,38 @@
 
     function createFunction () {
         function function1 () {
-            return 'Hello, world!';
+            return 'Hello, Lurker o_o...';
         }
         return function1();
     }
-    createFunction();
 
     
     const function1 = createFunction();
     // now we'll call the function we just created
-    function1; //should console.log('hello'); 
+    function1; //should console.log('hello');  
     console.log(createFunction());
 
 //  When you think you completed createFunction, un-comment out those lines in the code and run it to see if it works. 
 
 // Challenge 2
-// Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input that was used when the function was created. 
+// Create a function createFunctionPrinter that accepts one input and returns a function. When that created function is called, it should print out the input that was used when the function was created.
 
+// Given Variables
 var printSample = createFunctionPrinter('sample');
+var printHello = createFunctionPrinter('hello');
 
-var printHello = createFunctionPrinter('hello') 
+// My Answer
+function createFunctionPrinter(input){
+    // print out selected input
+    return function innerFunc(){
+        console.log(input);
+    }
+}
+printSample();
+printHello();
 // now we'll call the functions we just created printSample(); 
-// should console.log('sample'); printHello(); 
-// should console.log('hello'); 
+// should console.log('sample'); true
+// should console.log('hello'); true
 
 // Challenge 3
 // Examine the code for the outer function. Notice that we are returning a function and that function is using variables that are outside of its scope. Uncomment those lines of code. Try to deduce the output before executing. 
@@ -40,15 +49,21 @@ function outer() {
   return incrementCounter;
 }
 
-//var willCounter = outer();
-// var jasCounter = outer();
+var willCounter = outer();
+var jasCounter = outer();
 
 // Uncomment each of these lines one by one.
-// Before your do, guess what will be logged from each function call.
+// Before you do, guess what will be logged from each function call.
 
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+// 1
+willCounter();
+// 2
+willCounter();
+// 3
+jasCounter();
+// 1
+willCounter();
+// 4
 
-// jasCounter();
-// willCounter();
+//the counter functions increment each time it is called, and since will, and jas are two different counters they will only increment when they are called in that specific call, so if i call jasCounter once more, the counter of that function would be set to 2!
