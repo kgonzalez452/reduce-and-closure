f30ff4886eee6301218c661d5cbe266c689dc251
kgonzalez452 <kgonzalez@techlaunch.io>
2018-06-09 21:13:36 -0400

    finished challenge 4 on part2b. started challenge 5, struggling to figure it out

--- part2b/part2b.js
+++ part2b/part2b.js
@@ -2,6 +2,12 @@
 // Challenge 4
 // Now we are going to create a function addByX that returns a function that will add an input by x. 
 
+function addByX(input){
+    return function by(x){
+        console.log(input + x);
+    }
+}
+
 var addByTwo = addByX(2);
 addByTwo(1); //should return 3 
 addByTwo(2); //should return 4 
@@ -18,9 +24,34 @@ addByFour(10); //should return 14
 // Challenge 5
 // Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time, it should call the callback and return that output. If it is called any additional times, instead of calling the callback again it will simply return the output value from the first time it was called. 
 
+
+function once(input){
+    var result = 2;
+    function returnCall(){
+        result = null;
+        return result = input('here')
+        console.log(result += input );
+    };
+    return returnCall;
+};
+once(2);
+
+
+
+
+
 // Challenge 6
 // Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter. 
+
+function after(callbackNum, callback) {
+
+}
+
 
 // Challenge 7
 // Write a function delay that accepts a callback as the first parameter and the wait in milliseconds before allowing the callback to be invoked as the second parameter. Any additional arguments after wait are provided to func when it is invoked. 
 // HINT: research setTimeout(); 
+
+function delay (callback, setTimeout){
+
+}
